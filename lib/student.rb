7aa01e8f998d9d088d23c9d18bce482e64b5fcfc
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-03-25 05:38:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,7 +55,14 @@ class Student
   end
 
   def self.first_X_students_in_grade_10(num)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT ?
+    SQL
 
+    DB[:conn].execute(sql, num).map {|row| self.new_from_db(row)}
   end
 
   def save
