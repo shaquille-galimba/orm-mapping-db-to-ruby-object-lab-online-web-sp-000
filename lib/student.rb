394d6cc6ce03693ae05cb15dc8c0391923d8cc2f
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-03-25 05:39:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -73,7 +73,7 @@ class Student
       LIMIT 1
     SQL
 
-    DB[:conn].execute(sql).map {|row| self.new_from_db(row)}
+    DB[:conn].execute(sql).map {|row| self.new_from_db(row)}.first
   end
 
   def save
